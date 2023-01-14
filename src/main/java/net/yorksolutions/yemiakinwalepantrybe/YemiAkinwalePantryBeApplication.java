1a04776318c64f254e7394428d2a45f736f9ff76
York MacBook 031 <yorkmacbook031@YorkMacBook031.grandenetworks.net>
2023-01-14 10:52:58 -0600

    new files

---
+++ src/main/java/net/yorksolutions/yemiakinwalepantrybe/YemiAkinwalePantryBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.yemiakinwalepantrybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemiAkinwalePantryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YemiAkinwalePantryBeApplication.class, args);
+    }
+
+}
